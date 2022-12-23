8d0f94a3eb62650ed53c1b4d487920ab377a2f55
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2022-12-23 17:35:29 +0100

    Work

--- Conditionals.lua
+++ Conditionals.lua
@@ -1,12 +1,20 @@
-Conditionals = {}
+local addOnName = ...
+local version = '1.0.0'
 
-function Conditionals.doOnceWhen(areConditionsMet, fn)
-  local ticker
-  ticker = C_Timer.NewTicker(0, function()
-    if areConditionsMet() then
-      ticker:Cancel()
+if not Library.isRegistered(addOnName, version) then
+  --- @class Conditionals
+  local Conditionals = {}
 
-      fn()
-    end
-  end)
+  Library.register(addOnName, version, Conditionals)
+
+  function Conditionals.doOnceWhen(areConditionsMet, fn)
+    local ticker
+    ticker = C_Timer.NewTicker(0, function()
+      if areConditionsMet() then
+        ticker:Cancel()
+
+        fn()
+      end
+    end)
+  end
 end
