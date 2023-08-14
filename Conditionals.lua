a1090cff5caac7945dc990f7aefc27fab5c87226
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2023-08-14 17:09:27 +0200

    Work on using Library in library packages

--- Conditionals.lua
+++ Conditionals.lua
@@ -1,12 +1,24 @@
-Conditionals = {}
+local addOnName = 'Conditionals'
+local version = '2.0.0'
 
-function Conditionals.doOnceWhen(areConditionsMet, fn)
-  local ticker
-  ticker = C_Timer.NewTicker(0, function()
-    if areConditionsMet() then
-      ticker:Cancel()
+if _G.Library then
+  if not Library.isRegistered(addOnName, version) then
+    --- @class Conditionals
+    local Conditionals = {}
 
-      fn()
+    function Conditionals.doOnceWhen(areConditionsMet, fn)
+      local ticker
+      ticker = C_Timer.NewTicker(0, function()
+        if areConditionsMet() then
+          ticker:Cancel()
+
+          fn()
+        end
+      end)
     end
-  end)
+
+    Library.register(addOnName, version, Conditionals)
+  end
+else
+  error(addOnName + ' requires Library. It seems absent.')
 end
--- README.md
+++ README.md
@@ -6,4 +6,10 @@ A library with functionality for conditionally running code. This can help other
 
 * **Conditionals.doOnceWhen**: a function that runs a function once the given conditions are met.
 
+## How to use
+
+Please see "[How to use Library](https://github.com/SanjoSolutions/LuaLibrary#how-to-use)" for how to use this library.
+
+## Support
+
 You can support me on [Patreon](https://www.patreon.com/addons_by_sanjo).
